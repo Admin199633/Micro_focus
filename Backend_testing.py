4e59da091e0ad15f427364a40fcbb2a5c4bcd465
Admin199633 <108216254+Admin199633@users.noreply.github.com>
2022-06-26 12:59:35 +0300

    Update Backend_testing.py

--- Backend_testing.py
+++ Backend_testing.py
@@ -3,16 +3,18 @@ import requests
 user_id = "1"
 user_name = 'lior'
 
-resp = requests.post('http://127.0.0.1:5000/user/' + user_id + '', json={"user_name": "" + user_name + ""})
-print(resp)
-txt = resp.ok
-print(txt)
+
 
 
 resp = requests.get('http://localhost:5000/user/'+ user_id +'')
 txt = resp.content
 print(txt)
 
+
+# resp = requests.post('http://127.0.0.1:5000/user/' + user_id + '', json={"user_name": "" + user_name + ""})
+# print(resp)
+# txt = resp.ok
+# print(txt)
 
 # #
 # res = requests.put('http://127.0.0.1:5000/user/' + user_id + '')
